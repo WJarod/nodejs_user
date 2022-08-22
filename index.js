5d0355f5328061620b622cc21b03563173906fd7
Jarod Wuillaume <bwb.j.wuillaume@fondespierre.com>
2022-08-22 23:20:34 +0200

    fix_port

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(
 app.use('/', userRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.ddvsetx.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6666;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
